cbe47b821585d10e77a455a662b4d837c23098b5
yoshinaka-droid <yoshinaka012825@gmail.com>
2026-03-20 00:49:22 +0900

    import追加

--- src/main/java/com/example/shop/controller/ApiController.java
+++ src/main/java/com/example/shop/controller/ApiController.java
@@ -4,6 +4,7 @@ import com.example.shop.service.JsonDB;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api")
